3d1835853209c15e3e16ea8a354dec0c388b8b05
vikas <visparmar119@gmail.com>
2022-12-26 17:49:25 +0530

    adding spinner

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 
+
 const fetchCryptoDetail = async () => {
     try {
-        const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+        document.getElementById("div").style.visibility = "hidden"
+        document.getElementById("div2").style.visibility = "hidden"
+        var jioo= document.querySelector('.loding').style.display='flex';
         const response = await fetch(url, {
             method: "GET"
 
         });
 
         const data = await response.json();
-
-        //console.log(data[0]);
+        
+        var jioo= document.querySelector('.loding').style.display='none';  
+        document.getElementById("div").style.visibility = "visible"
+        document.getElementById("div2").style.visibility = "visible"
+        console.log("data");
         buildTable(data);
-
+        
+        
     } catch (error) {
         console.error(error);
 
     }
 }
-document.addEventListener('DOMContentLoaded', fetchCryptoDetail);
+document.addEventListener("DOMContentLoaded",fetchCryptoDetail);
 
 function buildTable(data) {
-
+    
     var table = document.getElementById('tablebody')
 
     for (var i = 0; i < data.length; i++) {
@@ -54,5 +62,6 @@ function buildTable(data) {
             </tr>`
             table.innerHTML+=row;
     }
+    // hide();
 }
 
